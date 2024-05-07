03f79ba7aaec30f5de97b82287dbdeebbd2c39d5
Prima Aji Akbar <prima.aji.tif23@polban.ac.id>
2024-05-07 11:06:39 +0700

    selasa

--- loginAdmin.cpp
+++ loginAdmin.cpp
@@ -3,3 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_USERNAME_LENGTH 50
+#define MAX_PASSWORD_LENGTH 50
+
+int loginAdmin(char *username, char *password) {
+    FILE *file = fopen("database/users.txt", "r");
+    if (file == NULL) {
+        printf("Gagal membuka file.\n");
+        return 0;
+    }
+    rewind(file);
+    
+    char storedUsername[MAX_USERNAME_LENGTH];
+    char storedPassword[MAX_PASSWORD_LENGTH];
+    
+    while (!feof(file)) {
+        fscanf(file, "%s %s %s\n", storedUsername, storedPassword, storedPin);
+        
+        if (strcmp(username, storedUsername) == 0 && strcmp(password, storedPassword) == 0) {
+            fclose(file);
+            printf("==================\n");
+            printf("= Login berhasil =\n");
+            printf("==================\n");
+            return 1; // Login berhasil
+        }
+    }
+    
+    printf("===============\n");
+    printf("= Login Gagal =\n");
+	printf("===============\n");
+    fclose(file);
+    return 0; // Login gagal
+}
